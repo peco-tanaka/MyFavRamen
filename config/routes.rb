5ee622098eb93b2de3ec1f57c1480e0a80004b7e
peco-tanaka <ok.computeruk1929@gmail.com>
2025-03-28 14:27:51 +0900

    homeコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
+  get "home/index"
+  devise_for :users
 
   root to: 'home#index'
 end
