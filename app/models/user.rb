dd2e9b865911a34203fb7c402b60a6de54f0604a
peco-tanaka <ok.computeruk1929@gmail.com>
2025-03-28 12:01:35 +0900

    userモデル　validation, associationの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :prefecture_id, presence: true
+
+  has_many :rankings
+  has_many :ranking_items, through: :rankings
+  has_many :favs
+  has_many :fav_shops, through: :favs, source: :shop
+  has_many :records
+  has_many :active_follows, class_name: "Follow", foreign_key: "follower_id"
+  has_many :passive_follows, class_name: "Follow", foreign_key: "followed_id"
+  has_many :following, through: :active_follows, source: :followed
+  has_many :followers, through: :passive_follows, source: :follower
+  belongs_to_active_hash :prefecture
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 end
