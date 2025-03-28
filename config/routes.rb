605cf3a15ea7f7e27d2956488160286e8aa1be04
peco-tanaka <ok.computeruk1929@gmail.com>
2025-03-28 16:51:26 +0900

    mend

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
 
-  resources :rankings, only: [:index, :show]
+  resources :rankings, only: [:index]
   resources :users, only: [:show]
   root to: 'home#index'
 end
