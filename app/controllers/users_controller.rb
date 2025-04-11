1f98adf221e995615c7a12d217ef75cd5e784ec4
peco-tanaka <ok.computeruk1929@gmail.com>
2025-04-11 12:50:15 +0900

    ルーティング、コントローラーアクションの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
+  !before_action :authenticate_user!, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update]
+
   def show
+    set_user
+  end
+
+  def edit
+    set_user
+  end
+
+  def update
+
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
 
-  
 end
