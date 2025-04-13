030afcd7e4d382e8aedf4799a89106145585e016
peco-tanaka <ok.computeruk1929@gmail.com>
2025-04-13 22:10:01 +0900

    rankingsコントローラーの雛形作成

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,8 +1,23 @@
 class RankingsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @rankings = current_user.rankings.includes(:genre)
+    # デフォルトで総合ランキングを表示
+    @current_ranking = @rankings.find_by(genre_id: 1)
+    @ranking_items = @current_ranking.ranking_items.includes(:shop).order(:position) if @current_ranking
   end
 
   def show
+
+  end
+
+  def edit
     @user = User.find(params[:id])
+    @rankings = @user.rankings
+  end
+
+  def update
+
   end
 end
