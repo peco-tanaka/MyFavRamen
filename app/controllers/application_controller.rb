2936de1b73f9366f9c9a32b40f92352258cb7c2f
peco-tanaka <ok.computeruk1929@gmail.com>
2025-04-16 03:05:52 +0900

    並べ替え機能の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  before_action :basic_auth, if: :production?
   allow_browser versions: :modern
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -15,5 +16,15 @@ class ApplicationController < ActionController::Base
 
   def after_sign_up_path_for(resource)
     rankings_path
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
+
+  def production?
+    Rails.env.production?
   end
 end
