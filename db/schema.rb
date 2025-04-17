88e0c3f13d01bb7238489df67c81acf0258a3ffe
peco-tanaka <ok.computeruk1929@gmail.com>
2025-04-17 21:48:09 +0900

    google_place_idをカラムに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_16_045039) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_17_123857) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,6 +76,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_16_045039) do
     t.string "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "google_place_id"
+    t.index ["google_place_id"], name: "index_shops_on_google_place_id"
   end
 
   create_table "users", force: :cascade do |t|
