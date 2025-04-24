3e1d94b70b75fa918e17d496196d18d2790f62d5
peco-tanaka <ok.computeruk1929@gmail.com>
2025-04-24 14:01:09 +0900

    Shopモデルに削除項目の関連付けを追加

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,5 @@
 class Shop < ApplicationRecord
-  has_many :ranking_items
+  has_many :ranking_items, dependent: :destroy
   has_many :rankings, through: :ranking_items
   has_many :favs
   has_many :users_who_fav, through: :favs, source: :user
